99ddc3fe9ee98b8b9dfb2b04ce4de8cf3bba50ca
dia_zx <dia_zx@mail.ru>
2022-09-29 05:21:36 +0300

    task 4 done

--- Lesson1/Task4/program.java
+++ Lesson1/Task4/program.java
@@ -9,9 +9,14 @@ import java.util.Scanner;
  */
 public class program {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("");
+        Scanner in = new Scanner(System.in, "866");
+        System.out.println("Введите n (n > 0)");
+        int n = in.nextInt();
+        if (n <= 0) {
+            System.out.println("Неверное значение n! (n > 0)");
+            return;
+        }
 
+        System.out.println("n-ое треугольное число: " + n * (n + 1) / 2);
     }
-
 }
