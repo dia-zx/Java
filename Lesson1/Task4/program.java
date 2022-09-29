d91d9b9e1d0026428d64b1762d7c9076ccd9028d
dia_zx <dia_zx@mail.ru>
2022-09-29 05:05:58 +0300

    tasks 1..3 done

---
+++ Lesson1/Task4/program.java
@@ -0,0 +1,17 @@
+// Вычислить n-ое треугольное число
+
+package Lesson1.Task4;
+
+import java.util.Scanner;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("");
+
+    }
+
+}
---
+++ Lesson1/Task5/program.java
@@ -0,0 +1,21 @@
+// Задано уравнение вида q + w = e, q, w, e >= 0.
+// Некоторые цифры могут быть заменены знаком вопроса,
+// например 2? + ?5 = 69.
+// Требуется восстановить выражение до верного равенства.
+// Предложить хотя бы одно решение или сообщить, что его нет.
+
+package Lesson1.Task5;
+
+import java.util.Scanner;
+
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("");
+
+    }
+
+}
